058989d7ce0b4f577834c850cbf208fca5551850
jamsrahimi <rahimi@protonmail.com>
2018-02-21 23:02:31 +0100

    2nd push for js3-week2

--- week2/spaAPI.js
+++ week2/spaAPI.js
@@ -1,17 +1,36 @@
 'use strict'
+// const h1 = document.createElement('h1');
+// document.body.appendChild(h1);
+// h1.innerHTML = 'HYF Repository';
 
-document.getElementById('button').addEventListener('click', loadText);
-function loadText() {
+// const div = document.createElement('div');
+// div.id = 'main';
+// document.body.appendChild(div);
+
+
+document.getElementById('button').addEventListener('click', loadRepos);
+function loadRepos() {
     console.log('You clicked me!');
     let xhr = new XMLHttpRequest();
     //    console.log(xhr);
     xhr.open('GET', 'https://api.github.com/orgs/HackYourFuture/repos', true);
     xhr.onload = function () {
         if (this.status == 200) {
-            document.getElementById('text').innerHTML = this.responseText;
-            //console.log(this.responseText);
+            let repos = JSON.parse(this.responseText);
+            let repoNames = "";
+            for (let i in repos) {
+                repoNames +=
+                    '<div class="namesOnly">' +
+                    '<ul>' + '<li>' + repos[i].name + '</li>' +
+                    '</ul>' +
+                    '</div>';
+    }
+            document.getElementById('text').innerHTML = repoNames;
+            // console.log(repos);
         }
     }
     xhr.send();
 
 }
+
+//for (let i in )
